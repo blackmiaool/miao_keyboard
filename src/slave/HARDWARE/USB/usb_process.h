18b408c73df4ba3ebe2d78ab3dc0aa72ed736223
blackmiaool <blackmiaool@gmail.com>
2018-01-14 11:19:58 +0800

    delete slave

--- src/slave/HARDWARE/USB/usb_process.h
+++
@@ -1,7 +0,0 @@
-#ifndef _USB_PROCESS_H_
-#define _USB_PROCESS_H_
-#include "stm32lib.h"
-#include "hw_config.h"
-void mouse_process(u8* buf);
-void keyborad_process(u8* buf);
-#endif//_USB_PROCESS_H_
